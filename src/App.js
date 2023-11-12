d2c40353f095064bfbbb34c94c20d05f0e9e854e
Hasan Talukder <mahadihasansiam2@gmail.com>
2023-11-12 12:55:01 +0600

    Featured done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import Featured from "./components/Featured";
 import TopNav from "./components/TopNav";
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <TopNav />
+      <Featured/>
       
     </div>
   );
